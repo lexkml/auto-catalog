b23bd8bdba03ef57c8e453b5d8f2c48503c48a0e
Alexandr Kamelchukov <lexkml@redcollar.ru>
2021-12-20 13:46:24 +0300

    ALEXPROJ-T-4 Добавление проекта в репозиторий

---
+++ src/main/java/com/example/autocatalog/AutoCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.autocatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autocatalog/AutoCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autocatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
